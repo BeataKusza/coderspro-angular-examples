c0c161999d79613b26055e9e4686af6565fd9e61
Beata.Kusza <anja37@o2.pl>
2017-04-01 16:37:16 +0200

    use ngRepeat directive for looping

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myAppController', ['$scope', function myAppController($scope) {
-  $scope.name = 'Kuba';
-
-  $scope.$watch('surname', function (newVal, oldVal) {
-    console.log('New value is ' + newVal);
-  });
-
-  setTimeOut(function () {
-    $scope.surname = 'Matyka';
-    $scope.greetings = 'Hello!';
-    $scope.$apply();
-  }, 1000)
-
+  $scope.names = ['Asia', 'Kasia', 'Zosia'];
 }]);
