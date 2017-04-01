95334667e465036ca2ad5f269129878315f53ff1
Beata.Kusza <anja37@o2.pl>
2017-04-01 16:28:40 +0200

    changing scope value

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var myApp = angular.module('myApp', []);
 
-myApp.contoroller('myAppController', ['$scope', function myAppController($scope) {
+myApp.controller('myAppController', ['$scope', function myAppController($scope) {
   $scope.name = 'Kuba';
-  $scope.surname = 'Matyka';
-  $scope.greetings = 'Hello!'
+
+  $scope.$watch('surname', function (newVal, oldVal) {
+    console.log('New value is ' + newVal);
+  });
+
+  setTimeOut(function () {
+    $scope.surname = 'Matyka';
+    $scope.greetings = 'Hello!';
+    $scope.$apply();
+  }, 1000)
+
 }]);
