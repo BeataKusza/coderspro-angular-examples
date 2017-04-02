c281a3b1a537910b63c8bbf78c5d32290b8dbec9
Beata.Kusza <anja37@o2.pl>
2017-04-02 13:55:34 +0200

    define routers: slice code

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-var myApp = angular.module('myApp', []);
+'use strict';
 
-myApp.controller('myAppCtrl', ['$scope', '$http', function myAppCtrl($scope, $http) {
+var countryApp = angular.module('countryApp', [
+  'ngRoute'
+]);
+
+countryApp.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    template: '<ul><li ng-repeat="country in countries">{{ country.name }}</li></ul>',
+    controller: 'CountryListCtrl'
+  })
+  .when('/:countryName', {
+    template: 'wait',
+    controller: 'CountryDetailCtrl'
+  })
+  .otherwise({
+    redirect: '/'
+  })
+}]);
+
+countryApp.controller('CountryListCtrl', ['$scope', '$http', function ($scope, $http) {
     $http.get('countries.json').success(function (data) {
       $scope.countries = data;
     });
+}]);
+
+countryApp.controller('CountryDetailCtrl', ['$scope', '$routeProvider', function ($scope, $routeProvider) {
+    console.log($routeProvider);
 }]);
